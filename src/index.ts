8bb9b017398b5b4f3c18080433bcb151054cb2b2
Nathan Storm <nathanstorm95@gmail.com>
2025-03-23 01:23:00 -0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Value Investing API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
